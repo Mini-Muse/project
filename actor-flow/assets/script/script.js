bd090349f7534e6afd6751bc1e567988c01464b2
giovannipro <g.profet@gmail.com>
2024-03-12 15:31:59 +0100

    load content in D3.js

--- actor-flow/assets/script/script.js
+++ actor-flow/assets/script/script.js
@@ -1,3 +1,11 @@
+colors = [
+    '#F0E3CB',
+    '#C9DFE5',
+    '#E0BBB6',
+    '#BAB7DE',
+    '#C1DDAB'
+] 
+
 function search_autocomplete(){
     const searchBox = document.getElementById('query_search');
     const autocompleteList = document.getElementById('autocomplete_list');
@@ -92,23 +100,56 @@ function timeline_labels() {
     }
 }
 
-function make_timeline(data,index){
+function make_timeline(box,data,index){
+    console.log(data[0].actions)
 
-    let selection = '#timeline_' + index
-    const timeline_box = document.getElementById('timeline_' + index)
-    let box_w = timeline_box.offsetWidth;
-    let box_h = timeline_box.offsetHeight;
+    // let selection = '#timeline_' + index
+    // const timeline_box = document.getElementById('timeline_' + index)
+    let box_w = box.offsetWidth;
+    let box_h = box.offsetHeight;
+    let margin = [10,20,10,20]
 
-    let svg = d3.select(selection)
+    let svg = d3.select(box)
         .append('svg')
-        .attr("width", box_w)
-        .attr("height", box_h);
+        .attr("width", box_w) // box_w - (margin[0] + margin[3])
+        .attr("height", box_h)
+        .attr("transform","translate(0,0)") // " + margin[0] + "," + margin[1] + ")"  
+
+    console.log(d3.select(box))
 
     let xScale = d3.scaleTime()
-        .domain([new Date("2022-01-01"), new Date("2022-12-31")])
-        .range([50, 350]);
+        .domain([new Date("1400-01-01"), new Date("2022-12-31")])
+        .range([0, box_w])
 
+    let xAxis = d3.axisBottom(xScale)
+        .ticks(5)
+        .tickFormat(d3.timeFormat("%Y"));
 
+    svg.append("g")
+        .attr("transform", "translate(0, 50)")
+        .call(xAxis);
+
+    svg.selectAll("circle")
+        .data(data.actions)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d){
+            console.log(d.date)
+            // xScale(new Date(d.date))
+            // console.log(d.date)
+        })
+        .attr("cy", 25)
+        .attr("r", 5)
+        .style("fill", "steelblue");
+
+    // svg.selectAll("text")
+    //     .data(data.actions)
+    //     .enter().append("text")
+    //     .attr("x", d => xScale(new Date(d.date)))
+    //     .attr("y", 75)
+    //     .text(d => d.name)
+    //     .style("text-anchor", "middle")
+    //     .style("font-size", "12px");
 }
 
 function load_data(){
@@ -117,6 +158,7 @@ function load_data(){
     const articles_count_box = document.getElementById('articles_count');
     const articles_actions_box = document.getElementById('articles_actions');
     let output = ''
+    let the_data;
 
     // fetch('https://minimuse.nlp.idsia.ch/actionflows/1',{ // ../../dummy-data/mini-muse-dummy-data.py
     //     method: 'GET',
@@ -154,96 +196,115 @@ function load_data(){
             // loop data
             filtered_data.forEach((item,i) => {
 
-                let bg = ''
-                let title = item.title;
-                let author = item.author;
-                let date = item.publication_date;
-                let actions = item.actions
-                let actions_count = item.actions.length
-                let link = '#'
-                let actor_id = 1
+                let container = d3.select('#articles_box')
 
-                if (i % 2 == 0) {
-                    bg = 'bg'
-                }
+                // list background
+                let list_item = container.append('li')
+                    .attr('class',function (d) {
+                        bg = ''
+                        if (i % 2 == 0) {
+                            bg = 'bg'
+                        }
+                        return bg
+                    })
 
-                output += '<li class="' + bg + '"">';
-                output += '<div class="article item">'
+                // article
+                let article_box = list_item.append('div')
+                    .attr('class','article item')
 
-                    output += '<div id="article_title">'
-                    output += '<a href="' + link + '">'
-                    output += title
-                    output += '</a>'
-                    output += '</div>'
-
-                    output += '<div id="article_author">'
-                    output += 'by ' + author
-                    output += '</div>'
-
-                    output += '<div id="article_date">'
-                    output += date
-                    output += '</div>'
-
-                output += '</div>'
-
-                actions.sort((a, b) => {
-                    return a.date - b.date;
-                });
-
-                // timeline
-                output += '<div id="timeline_' + i + '"class="article_timeline item" style="display: flex;">'
+                let article_title = article_box.append('div')
+                    .attr('id','article_title')
+                    .append('a')
+                    .attr('href',item.link)
+                    .append('text')
+                    .text(function(d){
+                        return item.title
+                    })
                 
+                let article_author = article_box.append('div')
+                    .attr('id','article_author')
+                    .append('text')
+                    .text(function(d){
+                        return 'by ' + item.author
+                    })
 
-                // colors = [
-                //     '#F0E3CB',
-                //     '#C9DFE5',
-                //     '#E0BBB6',
-                //     '#BAB7DE',
-                //     '#C1DDAB'
-                // ] 
+                let article_date = article_box.append('div')
+                    .attr('id','article_date')
+                    .append('text')
+                    .text(function(d){
+                        return item.publication_date
+                    })
 
-                
-                // for (let a = 0; a < actions.length; a++) {
+                // timeline ---------------
 
-                //     all_actions += a
+                let parseDate = d3.timeParse("%Y-%m-%d");
 
-                //     randomColor = colors[Math.floor(Math.random() * colors.length)];  
-                //     metadata = 'data-per="' + actor_id + '" data-art="' + i + '"' + 'data-act="' + a + '" '
-                //     metadata += 'data-tit="' + actions[a].title + '"'
-                //     metadata += 'data-dat="' + actions[a].date + '"'
-                //     metadata += 'data-loc="' + actions[a].location + '"'
-                
-                //     output += '<div class="act" ' + metadata + 'style="background-color:'+ randomColor +'; margin-left:' +  ((Math.random(10)*15) + 3)  +'%' + ';"></div>'
-                //     // console.log(randomColor)
-                // }
+                let box_w = '100%' //timeline_box.offsetWidth;
+                let box_h = '100px' //timeline_box.offsetHeight;
+                let margin = [10,20,10,20]
 
-                output += '</div>'
+                let xScale = d3.scaleTime()
+                    .domain([parseDate("1750-01-01"), parseDate("1890-12-31")])
+                    .range([margin[0], 800])
 
+                let timeline_box = list_item.append('div')
+                    .attr('id',function(d){
+                        return 'timeline_' + i 
+                    })
+                    .attr('class','article_timeline item')
 
-                output += '<div class="info_box item" id="info_box_' + i + '"></div>'
+                // timeline_container
+                let timeline_container = timeline_box.append('svg')
+                    .attr("width", box_w)
+                    .attr("height", box_h)
+                    .attr("transform","translate(0,0)") 
 
-                output += '</div>'
-                output += '</li>'
+                let actions_box = timeline_container.selectAll("g")
+                    .data(item.actions)
+                    .enter()
+                    .append("g")
+
+                let action_items = actions_box.append("rect")
+                    .attr("class", "timeline-point")
+                    .attr("x", function(d){
+
+                        if (d.date.length == 4){
+                            date_ = String(d.date) + '-01-01'
+                        }
+                        else {
+                            date_ = String(d.date)
+                        }                        
+                        console.log(date_)
+                        return xScale(parseDate(date_))
+
+                    })
+                    .attr("y",0)
+                    .attr("width",7)
+                    .attr("height",box_h)
+                    .attr("r", 5)
+                    .attr("data-date", function(d){
+                        let date_ = String(d.date)
+                        let date = xScale(new Date(date_)) //new Date() 
+                        
+                        return date_
+                    })
+                    .attr("fill",function(d){
+                        randomColor = colors[Math.floor(Math.random() * colors.length)]; 
+                        return randomColor
+                    })
+
+                let xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"));
+                timeline_container.append("g")
+                    .attr("transform", 'translate(0,80)')
+                    .attr("class","the_axis")
+                    .call(xAxis);
 
             });
 
-            // timeline
-            // filtered_data.forEach((item,i) => {
-            //     make_timelines(data,i)
-            // })
-
             articles_count_box.innerHTML = articles
-            title_box.innerHTML = output
             articles_actions_box.innerHTML = all_actions
 
-            // make timeline
-            timeline_boxes = document.getElementsByClassName('article_timeline');
-            for (let i = 0; i < timeline_boxes.length; i++) {
-                make_timeline(data,i)
-            }    
-            // console.log(data);
             // timeline_labels();
-            
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
