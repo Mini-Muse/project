4fa6ece42aed70898c0dfad5f1b617a683a4f803
giovannipro <g.profet@gmail.com>
2024-06-04 11:07:32 +0200

    show info about the selected article

--- article-inquiry/assets/script/script.js
+++ article-inquiry/assets/script/script.js
@@ -53,18 +53,16 @@ async function load_data(){
 
     list_articles(documents_data)
     get_statistics(actionflows_array)   
+    load_article_info(documents_data)
        
 }
 
 function list_articles(data){
     articles_box = document.getElementById('articles_box')
 
-    console.log(data)
-
     let output = ''
-
     data.forEach(item => {
-        output += '<div class="article_box">'
+        output += '<div class="article_box" data-id="' + item.document_id + '">'
         output += '<span class="article_title">' + item.title + '</span><br/>'
         output += '<span class="article_author">by ' + 'author' + ', </span>'
         output += '<span class="article_date">' + item.year + '</span>'
@@ -147,6 +145,54 @@ function get_statistics(data){
     timespan_actions.innerHTML = years;
 }
 
+function load_article_info(data){
+    console.log(data)
+
+    let id = data[0].document_id
+    
+    const article_item = document.querySelectorAll('.article_box')
+    const article_info_box = document.getElementById('article_info_box')
+
+    article_item.forEach(item => {
+        item.addEventListener('click', function() {
+            document_id = parseInt(item.getAttribute('data-id'))
+
+            article_item.forEach(item => {
+                item.classList.remove('selected');
+            })
+            item.classList.add('selected')
+
+            display_info(document_id)
+        })
+    })
+
+    function display_info(id){
+        let output = ''
+
+        data.forEach(item => {
+            if (item.document_id == id){
+
+                output += '<div id="the_title">' + item.title + '</div>'
+
+                output += '<div id="the_info">'
+                output += '<span>by author, </span>'
+                output += '<span>' + item.year + ', </span>'
+                output += '<span>' + item.issue + ', </span>'
+                output += '<span>' + item.volume + '</span>'
+                output += '</div>'
+
+                output += '<div id="the_abstract">'
+                output += '<h2>Abstract</h2>'
+                output += '<p>Abstract lorem ipsum ...</p>'
+                output += '</div>'
+
+                article_info_box.innerHTML = output
+            }
+        })
+    }
+    display_info(id)
+}
+
 function chat_with_NLP(){
     const chatBox = document.getElementById('question_chat_box');
     const theNLPBox = document.getElementById('reply_chat_box');
@@ -176,8 +222,6 @@ function chat_with_NLP(){
     }
 
     function load_NLP_reply() {
-        
-
         theNLPBox.innerHTML = 'message received ...'
     }
 
